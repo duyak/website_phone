ad06a8ad86b7cc33be40d29412ac32e6508e98f2
nguyen dang duy <nguyendangduynonglam@gmail.com>
2020-08-03 12:33:16 +0700

    Add project files.

---
+++ Website_StorePhone3/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website_StorePhone3.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        // GET: Admin/HomeAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult add_Product()
+        {
+            return View();
+        }
+        public ActionResult add_supplier()
+        {
+            return View();
+        }
+        public ActionResult add_user()
+        {
+            return View();
+        }
+        public ActionResult edit_supplier()
+        {
+            return View();
+        }
+        public ActionResult info_order()
+        {
+            return View();
+        }
+        public ActionResult manager_color()
+        {
+            return View();
+        }
+        public ActionResult manager_order()
+        {
+            return View();
+        }
+        public ActionResult manager_user()
+        {
+            return View();
+        }
+        public ActionResult profile_product()
+        {
+            return View();
+        }
+        public ActionResult profile_supplier()
+        {
+            return View();
+        }
+        public ActionResult profile_users()
+        {
+            return View();
+        }
+    }
+}
---
+++ Website_StorePhone3/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website_StorePhone3.Models.db;
+
+namespace Website_StorePhone3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult ListProduct()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult SingleBlog()
+        {
+            phone_storeEntities phone_Store = new phone_storeEntities();
+           
+            return View();
+        }
+        public ActionResult SingleProduct()
+        {
+            return View();
+        }
+       
+
+
+
+    }
+}
