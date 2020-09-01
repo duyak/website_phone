d70788d99746566b0c5ca30b44d30a3ebb799500
nguyen dang duy <nguyendangduynonglam@gmail.com>
2020-09-01 13:22:51 +0700

    category success

---
+++ Website_StorePhone3/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Website_StorePhone3.Models.DB;
+
+namespace Website_StorePhone3.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        dotnetstorephoneEntities db = new dotnetstorephoneEntities();
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            List<category> categories = db.categories.ToList();
+            return View(categories);
+        
+        }
+        public ActionResult AddCategory()
+        {
+            // Lấy data
+            // Lấy toàn bộ thể loại:
+            List<category> cate = db.categories.ToList();
+
+            // Tạo SelectList
+            SelectList cateList = new SelectList(cate, "id", "name");
+            // Set vào ViewBag
+            ViewBag.CategoryList = cateList;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory([Bind(Include = "id,activeFlag,createDate,updateDate")] category categorys,int parentId, string name, string code, string description)
+        {
+
+           
+
+            categorys.parentId = parentId;
+            categorys.name = name;
+            categorys.code = code;
+            categorys.description = description;
+            categorys.activeFlag = 1;
+            categorys.createDate = DateTime.Now;
+            categorys.updateDate = DateTime.Now;
+            db.categories.Add(categorys);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        // Get :Category/Delete
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+
+           
+            category modifyCategory = db.categories.Find(id);
+            if(modifyCategory == null)
+            {
+                return HttpNotFound();
+            }
+            modifyCategory.activeFlag = 0;
+            db.Entry(modifyCategory).State = EntityState.Modified;
+            db.SaveChanges();
+          
+
+            }catch(DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                /*    throw;*/
+
+            }
+            return RedirectToAction("Index");
+        }
+        // Get :Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            // Lấy data
+            // Lấy toàn bộ thể loại:
+            List<category> cate = db.categories.ToList();
+
+            // Tạo SelectList
+            SelectList cateList = new SelectList(cate, "id", "name");
+            // Set vào ViewBag
+            ViewBag.CategoryList = cateList;
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category category = db.categories.Find(id);
+            if(category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+        //POST: Category/Edit/5
+        [HttpPost]
+       
+        public ActionResult Edit([Bind(Include ="id,activeFlag,createDate,updateDate")] category category, int parentId, string name, string code, string description)
+        {
+
+            if (ModelState.IsValid)
+            {
+                category modifyCategory = db.categories.Find(category.id);
+                if(modifyCategory != null)
+                {
+                    if(parentId != 0)
+                    {
+                        modifyCategory.parentId = parentId;
+                        modifyCategory.name = name;
+                        modifyCategory.code = code;
+                        modifyCategory.description = description;
+                        modifyCategory.updateDate = DateTime.Now;
+
+                    }else
+                    {
+                        modifyCategory.name = name;
+                        modifyCategory.updateDate = DateTime.Now;
+
+                    }
+                }
+                db.Entry(modifyCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+
+        }
+    }
+        
+  
+
+    
+}
---
+++ Website_StorePhone3/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website_StorePhone3.Models.DB;
+
+namespace Website_StorePhone3.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        dotnetstorephoneEntities db = new dotnetstorephoneEntities();
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            List<product> products = db.products.ToList();
+            return View(products);
+        }
+        public ActionResult Add()
+        {
+            // Lấy data category
+            // Lấy toàn bộ thể loại:
+            List<category> cate = db.categories.ToList();
+
+            // Tạo SelectList
+            SelectList cateList = new SelectList(cate, "id", "name");
+            // Set vào ViewBag
+            ViewBag.CategoryList = cateList;
+
+            // Lấy data brand
+            // Lấy toàn bộ thể loại
+            List<brand> brand = db.brands.ToList();
+
+            //Tạo SelectList
+            SelectList brandList = new SelectList(brand, "id", "name");
+
+            //Set vào ViewBag
+            ViewBag.BrandList = brandList;
+
+
+            return View();
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add([Bind(Include ="id,activeFlag,createDate,updateDate")] product product,int catagoryId,int brandId,HttpPostedFileBase imgMain,string code,string description)
+        {
+            string fileName = System.IO.Path.GetFileName(imgMain.FileName);
+            string urlImage = Server.MapPath("~/Image/product/" + fileName);
+            imgMain.SaveAs(urlImage);
+            product.brandId = brandId;
+            product.catagoryId = catagoryId;
+            product.code = code;
+            product.description = description;
+            product.activeFlag = 1;
+            product.createDate = DateTime.Now;
+            product.updateDate = DateTime.Now;
+            db.products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
